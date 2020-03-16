12167450737b846e5b2bdcf497b1d5bb3a4da60b
Andrei Kulagin <kulawork23@yandex.ru>
2020-03-16 20:14:11 +0400

    change axios config

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     headers: {
-        common: {
-            "Content-Type": "application/json"
-        }
+        "Content-Type": "application/json"
     },
     baseURL: process.env.REACT_APP_BASE_URL
 });
